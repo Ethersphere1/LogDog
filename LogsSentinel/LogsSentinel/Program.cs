d969bc9b6a535e0be091aeb219c37f23b3bd908d
akshif <46319706+VoxelPixel@users.noreply.github.com>
2022-01-10 21:51:32 +0500

    project init

---
+++ LogsSentinel/LogsSentinel/Program.cs
@@ -0,0 +1,10 @@
+using LogsSentinel;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
