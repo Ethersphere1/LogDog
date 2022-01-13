d289edcfc2a925bda6a65ae854a572d909b4db98
akshif <46319706+VoxelPixel@users.noreply.github.com>
2022-01-13 22:30:24 +0500

    parsec task

--- LogsSentinel/LogsSentinel/ParsecLogsMonitor.cs
+++ LogsSentinel/LogsSentinel/ParsecLogsMonitor.cs
@@ -8,18 +8,69 @@ using Serilog.Events;
 
 namespace LogsSentinel
 {
-
-
     internal class ParsecLogsMonitor
     {
+        private static string appdataRoamingAddress = Environment.ExpandEnvironmentVariables(@"%AppData%");
+        private static string parsecLogFileAddress = $@"{appdataRoamingAddress}\Parsec\log.txt";
+        private List<string> parsecLogLastTwoLines;
+        private static int second = 1000;
+
         public ParsecLogsMonitor()
+        {
+            // checking if log.txt exists 
+            if (File.Exists(parsecLogFileAddress))
+            {
+                Log.Information($"log.txt found at {parsecLogFileAddress}");
+                ParsecLogFileTask();
+            }
+            else
+            {
+                Log.Information($"log.txt not found at {parsecLogFileAddress}");
+            }
+        } // ctor
+
+        public void ParsecLogFileTask()
         {
             while (true)
             {
-                Log.Information("this is from inside the parsecLog");
-                Thread.Sleep(3000);
 
+                if (File.ReadLines(parsecLogFileAddress).Count() < 2)
+                {
+                    // do nothing
+                    { }
+                }
+                else
+                {
+                    //creating another list to compare logs after 10 seconds 
+                    List<string> tempLastTwoLines = File.ReadLines(parsecLogFileAddress).Reverse().Take(2).Reverse().ToList();
+
+                    //create/delete 1.txt temp file
+                    //string tempPath = @"C:\temp\1.txt";
+                    string tempPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), @"temp\parsec.txt");
+                    if (tempLastTwoLines[0] == parsecLogLastTwoLines[0]
+                        && tempLastTwoLines[1] == parsecLogLastTwoLines[1]
+                        || !File.Exists(parsecLogFileAddress)
+                        )
+                    {
+                        if (!File.Exists(tempPath))
+                        {
+                            Thread.Sleep(500);
+                            //StreamWriter streamWriter = new StreamWriter(@"C:\temp\1.txt");
+                            StreamWriter streamWriter = new StreamWriter(tempPath);
+                            Thread.Sleep(500);
+                            parsecLogLastTwoLines = File.ReadLines(parsecLogFileAddress).Reverse().Take(2).Reverse().ToList();
+                            streamWriter.Close();
+                        }
+                    } // if
+                    else
+                    {
+                        File.Delete(tempPath);
+                        parsecLogLastTwoLines = File.ReadLines(parsecLogFileAddress).Reverse().Take(2).Reverse().ToList();
+                    } // if-else
+                }
+                Thread.Sleep(second * 10);
             }
-        } // ctor
+        } // LogFileTaskInit
+
     } // class
 } // namespace
