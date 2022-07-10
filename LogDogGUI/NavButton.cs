793f66200891e0896dafba4b1cab24ee7572d425
akshif <46319706+VoxelPixel@users.noreply.github.com>
2022-07-10 20:25:40 +0500

    added icon code

---
+++ LogDogGUI/NavButton.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LogDogGUI
+{
+    
+    public class NavButton : ListBoxItem
+    {
+        static NavButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NavButton), new FrameworkPropertyMetadata(typeof(NavButton)));
+        }
+
+
+
+        // to hold information about how to find a specific page within the application
+        public Uri NavLink
+        {
+            get { return (Uri)GetValue(NavLinkProperty); }
+            set { SetValue(NavLinkProperty, value); }
+        }
+        public static readonly DependencyProperty NavLinkProperty =
+            DependencyProperty.Register("NavLink", typeof(Uri), typeof(NavButton), new PropertyMetadata(null));
+
+
+        // hold information about icon
+        public Geometry Icon
+        {
+            get { return (Geometry)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(Geometry), typeof(NavButton), new PropertyMetadata(null));
+
+
+
+
+    } // class
+} // namespace
